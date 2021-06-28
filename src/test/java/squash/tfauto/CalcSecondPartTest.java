e740d4f87355028ab2984d4921432be0c4b2f121
tdemijolla <tdemijolla@henix.fr>
2021-06-28 18:46:56 +0200

    Updated Test

--- src/test/java/squash/tfauto/CalcSecondPartTest.java
+++ src/test/java/squash/tfauto/CalcSecondPartTest.java
@@ -1,8 +1,8 @@
 package squash.tfauto;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.opentestfactory.util.ParameterService;
 import org.opentestfactory.util.ParameterServiceImpl;
 
 import java.io.IOException;
@@ -15,10 +15,9 @@ public class CalcSecondPartTest {
 
     @Test
     public void sub() throws IOException {
-
-        var1 = Integer.parseInt(ParameterServiceImpl.getInstance().getParameter("DS_VAR1"));
-        var2 = Integer.parseInt(ParameterServiceImpl.getInstance().getParameter("DS_VAR2"));
-        res = Integer.parseInt(ParameterServiceImpl.getInstance().getParameter("DS_RES"));
+        var1 = Integer.parseInt(ParameterService.INSTANCE.getParameter("DS_VAR1"));
+        var2 = Integer.parseInt(ParameterService.INSTANCE.getParameter("DS_VAR2"));
+        res = Integer.parseInt(ParameterService.INSTANCE.getParameter("DS_RES"));
         Assertions.assertTrue((var1-var2)==res, "Le résultat du calcul est incorrect.");
     }
 }
