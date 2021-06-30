7454d12d2d714bee4366cc19c9d92476894e05ba
tdemijolla <tdemijolla@henix.fr>
2021-06-30 12:12:01 +0200

    Added Test with TC CUF and default value

--- src/test/java/squash/tfauto/ParameterTest.java
+++ src/test/java/squash/tfauto/ParameterTest.java
@@ -3,21 +3,32 @@ package squash.tfauto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.opentestfactory.util.ParameterService;
-import org.opentestfactory.util.ParameterServiceImpl;
 
-import java.io.IOException;
-
-public class CalcSecondPartTest {
+public class ParameterTest {
 
     int var1;
     int var2;
     int res;
 
+    String label;
+
     @Test
-    public void sub() throws IOException {
+    public void sub() {
         var1 = Integer.parseInt(ParameterService.INSTANCE.getParameter("DS_VAR1"));
         var2 = Integer.parseInt(ParameterService.INSTANCE.getParameter("DS_VAR2"));
         res = Integer.parseInt(ParameterService.INSTANCE.getParameter("DS_RES"));
         Assertions.assertTrue((var1-var2)==res, "Le résultat du calcul est incorrect.");
+    }
+
+    @Test
+    public void display() {
+        label = ParameterService.INSTANCE.getParameter("TC_CUF_label");
+        Assertions.assertTrue(label.equals("Squash"), "Le label est incorrect.");
+    }
+
+    @Test
+    public void displayWithDefaultValue() {
+        label = ParameterService.INSTANCE.getParameter("TC_CUF_fake_label", "Squash");
+        Assertions.assertTrue(label.equals("Squash"), "Le label est incorrect.");
     }
 }
