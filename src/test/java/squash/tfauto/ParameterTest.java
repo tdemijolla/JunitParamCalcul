a716f98196c6ed08f50757effcc6ba309bc1aba4
tdemijolla <tdemijolla@henix.fr>
2021-07-08 10:23:49 +0200

    Adapted tests to the library changes

--- src/test/java/squash/tfauto/ParameterTest.java
+++ src/test/java/squash/tfauto/ParameterTest.java
@@ -3,6 +3,7 @@ package squash.tfauto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.opentestfactory.exception.ParameterException;
+import org.opentestfactory.exception.ParameterFormatException;
 import org.opentestfactory.util.ParameterService;
 
 public class ParameterTest {
@@ -11,15 +12,15 @@ public class ParameterTest {
     @Test
     public void sub() throws ParameterException {
         int var1 = ParameterService.INSTANCE.getInt("DS_VAR1");
-        int var2 = ParameterService.INSTANCE.getIntTestParam("DS_VAR2");
-        int res = ParameterService.INSTANCE.getIntGlobalParam("DS_RES");
+        int var2 = ParameterService.INSTANCE.getTestInt("DS_VAR2");
+        int res = ParameterService.INSTANCE.getGlobalInt("DS_RES");
         Assertions.assertEquals(res, var1-var2, "Wrong calculation result");
     }
     @Test
-    public void add() {
+    public void add() throws ParameterFormatException {
         float var1 = ParameterService.INSTANCE.getFloat("DS_FAKE_VAR1", 42.21f);
-        float var2 = ParameterService.INSTANCE.getFloatTestParam("DS_FAKE_VAR2", 15.23f);
-        float res = ParameterService.INSTANCE.getFloatGlobalParam("DS_FAKE_RES",57.44f );
+        float var2 = ParameterService.INSTANCE.getTestFloat("DS_FAKE_VAR2", 15.23f);
+        float res = ParameterService.INSTANCE.getGlobalFloat("DS_FAKE_RES",57.44f );
         Assertions.assertEquals(res, var1+var2, "Wrong calculation result");
     }
 
@@ -30,7 +31,7 @@ public class ParameterTest {
         if(isDisplay){
             label = ParameterService.INSTANCE.getString("TC_CUF_LABEL");
         }
-        Assertions.assertEquals(label, "Squash", "Wrong calculation result");
+        Assertions.assertEquals(label, "Squash", "Wrong label displayed");
     }
 
 }
